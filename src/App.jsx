6df67b421739be4d8ce0118b7e6ee7bb8e556357
YoungJin <oyj921205@gmail.com>
2025-01-21 15:31:31 +0900

    initiate Project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = function () {
+  return (
+    <div style={styles.container}>
+      <h1>Medal Tracker</h1>
+      {/* 입력 폼 컴포넌트 */}
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}
+
+export default App
